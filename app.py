a5fdc9351cc63dbb82973a67f7ded54ee2dedab9
Naomi <naomismith97@yahoo.co.uk>
2024-10-22 12:36:53 +0100

    added /wave

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def hello():
     name = request.args['name'] # The value is 'David'
 
     # Send back a friendly greeting with the name
-    return f"Hello {name}!"
+    return f"Hello {name}!\n"
 
 # To make a request, run:
 # curl "http://localhost:5001/hello?name=David"
@@ -49,7 +49,7 @@ def goodbye():
     name = request.form['name'] # The value is 'Alice'
 
     # Send back a fond farewell with the name
-    return f"Goodbye {name}!"
+    return f"Goodbye {name}!\n"
 
 # To post, run:
 #  curl -X POST -d "name=Alice" http://localhost:5001/goodbye
@@ -63,10 +63,26 @@ def goodbye():
 def submit():
     name = request.form['name'] # Leo
     message = request.form['message'] # Hello world
-    return f'Thanks {name}, you sent this message: "{message}'
+    return f'Thanks {name}, you sent this message: "{message}"\n'
 
 # To post, run:
 #  curl -X POST -d "name=Leo&message=Hello world" http://localhost:5001/submit
+
+
+# Request:
+# GET /wave?name=Leo
+#  with body paremeter: name=Leo
+# Expected response (200 OK):
+# I am waving at Leo
+
+@app.route('/wave', methods=['POST'])
+def wave():
+    name = request.form['name'] # Leo
+    return f"I am waving at {name}\n"
+
+# To post, run:
+# curl -X POST -d "name=Leo" http://localhost:5001/wave
+
 
 
 # These lines start the server if you run this file directly

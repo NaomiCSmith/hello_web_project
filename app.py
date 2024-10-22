80cdbea64388a1bb5f548d446463ad898e45ade7
Naomi <naomismith97@yahoo.co.uk>
2024-10-22 13:55:04 +0100

    changed /wave from POST to GET

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def hello():
     name = request.args['name'] # The value is 'David'
 
     # Send back a friendly greeting with the name
-    return f"Hello {name}!\n"
+    return f"Hello {name}!"
 
 # To make a request, run:
 # curl "http://localhost:5001/hello?name=David"
@@ -49,7 +49,7 @@ def goodbye():
     name = request.form['name'] # The value is 'Alice'
 
     # Send back a fond farewell with the name
-    return f"Goodbye {name}!\n"
+    return f"Goodbye {name}!"
 
 # To post, run:
 #  curl -X POST -d "name=Alice" http://localhost:5001/goodbye
@@ -63,7 +63,7 @@ def goodbye():
 def submit():
     name = request.form['name'] # Leo
     message = request.form['message'] # Hello world
-    return f'Thanks {name}, you sent this message: "{message}"\n'
+    return f'Thanks {name}, you sent this message: "{message}"'
 
 # To post, run:
 #  curl -X POST -d "name=Leo&message=Hello world" http://localhost:5001/submit
@@ -75,10 +75,10 @@ def submit():
 # Expected response (200 OK):
 # I am waving at Leo
 
-@app.route('/wave', methods=['POST'])
+@app.route('/wave', methods=['GET'])
 def wave():
-    name = request.form['name'] # Leo
-    return f"I am waving at {name}\n"
+    name = request.args['name'] # Leo
+    return f"I am waving at {name}"
 
 # To post, run:
 # curl -X POST -d "name=Leo" http://localhost:5001/wave
